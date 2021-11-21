25a574e07921ac9563fb32846d9fdd2ecb9a121f
Daniel Papp <danipoker1@gmail.com>
2021-11-21 14:29:30 +0100

    ready to fix scrolldown in hero

--- wp-content/themes/analog/app/js/script.js
+++ wp-content/themes/analog/app/js/script.js
@@ -21,3 +21,17 @@ btnHamburger.addEventListener("click", function () {
     headerMenu.style.opacity = 1;
   }
 });
+
+var map = L.map("map").setView([55.3930661, 10.3943626], 16);
+
+L.tileLayer(
+  "https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey=1ae607e81bdf4511aa65fdb2c807ab07",
+  {
+    attribution: "© OpenStreetMap contributors",
+  }
+).addTo(map);
+
+L.marker([55.3930661, 10.3943626])
+  .addTo(map)
+  .bindPopup("Find os her! :)")
+  .openPopup();
--- wp-content/themes/analog/header.php
+++ wp-content/themes/analog/header.php
@@ -8,6 +8,12 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet">
+ <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+   integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+   crossorigin=""/>
+   <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+   integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+   crossorigin=""></script>
   <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory'); ?>/styles/reset.css">
   <title><?php echo get_bloginfo('name'); ?></title>
 <?php wp_head();
