14d76825af9db044aa989e3548e8b59955ac84a3
ssssskull <31503598+ssssskull@users.noreply.github.com>
2021-11-29 13:07:15 +0100

    404 height fucked

--- wp-content/themes/analog/public/js/script.js
+++ wp-content/themes/analog/public/js/script.js
@@ -1,1 +1,256 @@
-(()=>{"use strict";var e,o={788:()=>{console.log("Hej smukke!");var e=document.querySelector("header"),o=document.querySelector("nav"),t=document.querySelector("body"),r=document.querySelector("#btnHamburger"),n=document.querySelector(".header__menuwrapper"),a=document.querySelector(".hero__indicator");r.addEventListener("click",(function(){e.classList.contains("open")?(e.classList.remove("open"),o.classList.remove("nav-open"),t.style.overflow="auto",n.style.opacity=0):(e.classList.add("open"),o.classList.add("nav-open"),t.style.overflow="hidden",n.style.opacity=1)}));var i=window.location.pathname;if(i.includes("find-os")){var s=L.map("map").setView([55.3930661,10.3943626],16);L.tileLayer("https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey=1ae607e81bdf4511aa65fdb2c807ab07").addTo(s),L.marker([55.3930661,10.3943626]).addTo(s).bindPopup("Analog kaffebar <br> Albanigade 54c ☕").openPopup()}if(document.body.contains(a)&&a.addEventListener("click",(function(e){e.preventDefault(),document.querySelector("#about").scrollIntoView({behavior:"smooth"})})),"grid"===window.getComputedStyle(r).getPropertyValue("display")){var c=document.querySelector(".hero__container");new IntersectionObserver((function(e,t){e.forEach((function(e){e.isIntersecting?o.classList.remove("nav-open-observer"):o.classList.add("nav-open-observer")}))}),{root:null,threshold:.85}).observe(c)}if(!i.includes("find-os")&&!i.includes("404")&&!i.includes("menukort")){var d=document.createElement("div");d.className="video-modal";var l=document.createElement("div");l.className="video-overlay",d.insertAdjacentHTML("beforeend",'<iframe src="https://www.youtube.com/embed/qwvxtBlzbO4?vq=hd1080&autoplay=1&modestbranding=1&showinfo=0&rel=0&iv_load_policy=3" width="560" height="315" frameborder="0"></iframe>'),document.querySelector("#video-listener").addEventListener("click",(function(){t.insertAdjacentElement("beforeend",d),t.insertAdjacentElement("beforeend",l),l.classList.add("overlay-visible"),setTimeout((function(){d.classList.add("modal-visible")}),100)})),l.addEventListener("click",(function(){setTimeout((function(){d.remove(),l.remove(),d.classList.remove("video-close"),d.classList.remove("modal-visible")}),700),d.classList.add("video-close")}))}},990:()=>{}},t={};function r(e){var n=t[e];if(void 0!==n)return n.exports;var a=t[e]={exports:{}};return o[e](a,a.exports,r),a.exports}r.m=o,e=[],r.O=(o,t,n,a)=>{if(!t){var i=1/0;for(l=0;l<e.length;l++){for(var[t,n,a]=e[l],s=!0,c=0;c<t.length;c++)(!1&a||i>=a)&&Object.keys(r.O).every((e=>r.O[e](t[c])))?t.splice(c--,1):(s=!1,a<i&&(i=a));if(s){e.splice(l--,1);var d=n();void 0!==d&&(o=d)}}return o}a=a||0;for(var l=e.length;l>0&&e[l-1][2]>a;l--)e[l]=e[l-1];e[l]=[t,n,a]},r.o=(e,o)=>Object.prototype.hasOwnProperty.call(e,o),(()=>{var e={42:0,938:0};r.O.j=o=>0===e[o];var o=(o,t)=>{var n,a,[i,s,c]=t,d=0;if(i.some((o=>0!==e[o]))){for(n in s)r.o(s,n)&&(r.m[n]=s[n]);if(c)var l=c(r)}for(o&&o(t);d<i.length;d++)a=i[d],r.o(e,a)&&e[a]&&e[a][0](),e[i[d]]=0;return r.O(l)},t=self.webpackChunkanalog=self.webpackChunkanalog||[];t.forEach(o.bind(null,0)),t.push=o.bind(null,t.push.bind(t))})(),r.O(void 0,[938],(()=>r(788)));var n=r.O(void 0,[938],(()=>r(990)));n=r.O(n)})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./app/js/script.js":
+/*!**************************!*\
+  !*** ./app/js/script.js ***!
+  \**************************/
+/***/ (() => {
+
+
+
+console.log("Hej smukke!");
+var header = document.querySelector("header");
+var nav = document.querySelector("nav");
+var body = document.querySelector("body");
+var btnHamburger = document.querySelector("#btnHamburger");
+var headerMenu = document.querySelector(".header__menuwrapper");
+var scrollIndicator = document.querySelector(".hero__indicator");
+btnHamburger.addEventListener("click", function () {
+  if (header.classList.contains("open")) {
+    /* Luk hamburger menu
+    ============================*/
+    header.classList.remove("open");
+    nav.classList.remove("nav-open");
+    body.style.overflow = "auto";
+    headerMenu.style.opacity = 0;
+  } else {
+    /* Åben hamburger menu
+    ============================*/
+    header.classList.add("open");
+    nav.classList.add("nav-open");
+    body.style.overflow = "hidden";
+    headerMenu.style.opacity = 1;
+  }
+});
+var path = window.location.pathname;
+
+if (path.includes("find-os")) {
+  var map = L.map("map").setView([55.3930661, 10.3943626], 16);
+  L.tileLayer("https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey=1ae607e81bdf4511aa65fdb2c807ab07").addTo(map);
+  L.marker([55.3930661, 10.3943626]).addTo(map).bindPopup("Analog kaffebar <br> Albanigade 54c \u2615").openPopup();
+}
+
+if (document.body.contains(scrollIndicator)) {
+  scrollIndicator.addEventListener("click", function (e) {
+    e.preventDefault();
+    var targetScroll = document.querySelector("#about");
+    targetScroll.scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+}
+
+if (window.getComputedStyle(btnHamburger).getPropertyValue("display") === "grid") {
+  var section = document.querySelector(".hero__container");
+  var options = {
+    root: null,
+    // It is in the viewport
+    threshold: 0.85
+  };
+  var observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(function (entry) {
+      if (!entry.isIntersecting) {
+        nav.classList.add("nav-open-observer");
+      } else {
+        nav.classList.remove("nav-open-observer");
+      }
+    });
+  }, options);
+  observer.observe(section);
+} // Video modal
+
+
+if (!path.includes("find-os") && !path.includes("404") && !path.includes("menukort")) {
+  var videoModal = document.createElement("div");
+  videoModal.className = "video-modal";
+  var videoOverlay = document.createElement("div");
+  videoOverlay.className = "video-overlay";
+  videoModal.insertAdjacentHTML("beforeend", '<iframe src="https://www.youtube.com/embed/qwvxtBlzbO4?vq=hd1080&autoplay=1&modestbranding=1&showinfo=0&rel=0&iv_load_policy=3" width="560" height="315" frameborder="0"></iframe>');
+  document.querySelector("#video-listener").addEventListener("click", function () {
+    body.insertAdjacentElement("beforeend", videoModal);
+    body.insertAdjacentElement("beforeend", videoOverlay);
+    videoOverlay.classList.add("overlay-visible");
+    setTimeout(function () {
+      videoModal.classList.add("modal-visible");
+    }, 100);
+  });
+  videoOverlay.addEventListener("click", function () {
+    setTimeout(function () {
+      videoModal.remove();
+      videoOverlay.remove();
+      videoModal.classList.remove("video-close");
+      videoModal.classList.remove("modal-visible");
+    }, 700);
+    videoModal.classList.add("video-close");
+  });
+}
+
+/***/ }),
+
+/***/ "./app/scss/style.scss":
+/*!*****************************!*\
+  !*** ./app/scss/style.scss ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	(() => {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = (result, chunkIds, fn, priority) => {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var [chunkIds, fn, priority] = deferred[i];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"/js/script": 0,
+/******/ 			"css/style": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		__webpack_require__.O.j = (chunkId) => (installedChunks[chunkId] === 0);
+/******/ 		
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			if(chunkIds.some((id) => (installedChunks[id] !== 0))) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
+/******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
+/******/ 			}
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for(;i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+/******/ 				}
+/******/ 				installedChunks[chunkIds[i]] = 0;
+/******/ 			}
+/******/ 			return __webpack_require__.O(result);
+/******/ 		}
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunkanalog"] = self["webpackChunkanalog"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	__webpack_require__.O(undefined, ["css/style"], () => (__webpack_require__("./app/js/script.js")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/style"], () => (__webpack_require__("./app/scss/style.scss")))
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+/******/ 	
+/******/ })()
+;
