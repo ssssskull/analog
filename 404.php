684fad1a8e8e1e87dbcc4b91ba60540a269d54f9
Daniel Papp <danipoker1@gmail.com>
2021-12-08 12:34:52 +0100

    404 done

--- 404.php
+++ 404.php
@@ -3,9 +3,12 @@ get_header();?>
 <section class="hero">
     <div class="hero__container container">
             <div class="hero__content container--px">
-                <h1 class="hero__title">Hov...</h1>
-                <p class="hero__subheading">Beklager, vi kan ikke finde den side, som du leder efter. Dette kan skyldes at den er blevet fjernet eller flyttet.</p>
-            <a class="btn btn--primary" href="<?php echo home_url()?>">Tilbage til forsiden</a>
+                <h1 class="hero__title">Intet at se her!</h1>
+                <p class="hero__subheading">Vores fejl – siden, du leder efter, er nok blevet slettet eller flyttet. Men vi kan sagtens hjælpe dig videre herfra. Du kan gå tilbage til forsiden og starte forfra, eller ledte du måske efter vores menu?</p>
+            <div class="notfound__btns">
+                <a class="btn btn--primary" href="<?php echo home_url()?>">Tilbage til forsiden</a>
+                <a class="btn btn--tertiary" href="<?php echo home_url()?>/menu">Til menuen</a>
+            </div>
             </div>
     </div>
 </section>
