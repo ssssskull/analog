eaac61938cea391f35da4ff3b62c8aaf614bf797
Daniel Papp <danipoker1@gmail.com>
2021-12-10 11:31:50 +0100

    Workbox last fix

--- public/js/serviceWorker.js
+++ public/js/serviceWorker.js
@@ -1,8 +1,11 @@
 importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js"
 );
-
 workbox.routing.registerRoute(
-  new RegExp("/.*"),
+  /\.(?:js|css|webp|png|svg)$/,
+  new workbox.strategies.StaleWhileRevalidate()
+);
+workbox.routing.registerRoute(
+  /(\/|\.html)$/,
   new workbox.strategies.NetworkFirst()
 );
