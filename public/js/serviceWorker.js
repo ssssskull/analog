68570cc4963dbd345873817022de7d91f5d1c604
Daniel Papp <danipoker1@gmail.com>
2021-12-10 10:41:31 +0100

    fixed 404 for single post type, playing with service workers

--- public/js/serviceWorker.js
+++ public/js/serviceWorker.js
@@ -1,3 +1,12 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/public/js/serviceWorker.js");
 }
+
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js"
+);
+
+workbox.routing.registerRoute(
+  ({ request }) => request.destination === "public/images",
+  new workbox.strategies.NetworkFirst()
+);
---
+++ single-panini.php
@@ -0,0 +1,2 @@
+<meta name="robots" content="none, nosnippet, noarchive" />
+<?php wp_redirect( get_template_directory_uri() . '404.php' ); exit; ?>
