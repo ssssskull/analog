f2b9b249a69a35d357389fb9b27811a838abf0a7
Daniel Papp <danipoker1@gmail.com>
2021-12-10 10:56:25 +0100

    testing serviceworker

--- public/js/serviceWorker.js
+++ public/js/serviceWorker.js
@@ -1,7 +1,3 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/public/js/serviceWorker.js");
-}
-
 importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js"
 );
